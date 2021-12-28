a30ae48bc2a7f2950b9976b3cf3768c837dee4e9
circonfl3x <sayalelowesi456@gmail.com>
2021-12-28 13:03:03 +0300

    Piri Overhaul
    - Completely rewritten backend
    
    - Deprecated most of the older scripts and directories in favor of newer ones
    
    - Added notification prompts to Piri
    
    - Added an install/check dependency script and an installer and uninstaller file
    
    - Slightly modified the gambas source to include a piri button that functions as an on/off

--- Recode/tests/t40_combine.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import common
-from common import setup_module, teardown_module
-
-def test_1():
-    # That combine does not crash.
-    common.request('co..l1')
-    common.validate('', '')
